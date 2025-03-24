3973d7b4241fc4bcf296d9995a22a8c3cbaf1b04
Naman Doshi <160260985+namandoshi9@users.noreply.github.com>
2025-03-24 12:39:37 +0530

    Delete crystalevents directory

--- crystalevents/crystal_admin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrystalAdminConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "crystal_admin"
--- crystalevents/crystal_client/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrystalClientConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "crystal_client"
